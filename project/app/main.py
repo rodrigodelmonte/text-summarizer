ac2c33f7999336942647c73a6334520c06036cd6
rodrigodelmonte <rodrigo.monte@onefootball.com>
2021-04-20 22:06:52 +0200

    Configure postgres and tortoise-orm

--- project/app/main.py
+++ project/app/main.py
@@ -1,11 +1,20 @@
 # project/app/main.py
-
+import os
 
 from fastapi import FastAPI, Depends
+from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import get_settings, Settings
 
 app = FastAPI()
+
+register_tortoise(
+    app=app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
 
 
 @app.get("/ping")
@@ -13,5 +22,5 @@ async def pong(settings: Settings = Depends(get_settings)):
     return {
         "ping": "pong!",
         "environment": settings.environment,
-        "testing": settings.testing
+        "testing": settings.testing,
     }
